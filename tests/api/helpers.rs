c24391c5a41bc66f8da737b861cefa185503b241
Your Name <you@example.com>
2021-11-11 21:22:34 +0900

    bug fix on test trace

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -15,10 +15,10 @@ static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
     if std::env::var("TEST_LOG").is_ok() {
-        let subscriber = get_subscriber(default_filter_level, subscriber_name, std::io::stdout);
+        let subscriber = get_subscriber(subscriber_name, default_filter_level,  std::io::stdout);
         init_subscriber(subscriber);
     } else {
-        let subscriber = get_subscriber(default_filter_level, subscriber_name, std::io::sink);
+        let subscriber = get_subscriber(subscriber_name, default_filter_level,  std::io::sink);
         init_subscriber(subscriber);
     }
 });
